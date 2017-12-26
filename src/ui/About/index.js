beb6c107cc648da60ba46591dbfaff28760f1f08
Ryan Hansen <rphansen91@gmail.com>
2017-12-26 12:30:27 -0800

    Social plugin

--- src/ui/About/index.js
+++ src/ui/About/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Typography from 'material-ui/Typography';
 import SEO from '../SEO';
+import Social from '../Social';
 import './style.css';
 
 export default () =>
   <div>
     <SEO title={'About | Hodl Stream'} path='/about' />
     <section />
-    <section>
-      <Typography type="title">About</Typography>
+    <section className="about">
+      <div className="about-content">
+        <Typography type="title">About</Typography>
+      </div>
+      <div className="about-sidebar">
+        <Social page="hodlstream" name="Hodl Stream" tabs="timeline, events" />
+      </div>
     </section>
   </div>
---
+++ src/ui/Social/index.js
@@ -0,0 +1,43 @@
+/* global FB */
+import React, { Component } from 'react';
+
+function loadScript (src) {
+  return new Promise(function (res, rej) {
+    const script = document.createElement('script')
+    script.src = src
+    script.onload = res
+    script.onerror = rej
+    document.body.appendChild(script)
+  })
+}
+
+export default class extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentWillMount () {
+    console.log('MOUNTING')
+    loadScript('https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9')
+    .then(() => {
+      console.log('LOADED')
+      this.setState({ loaded: true })
+      FB.XFBML.parse()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  render () {
+    const { page, name, tabs='timeline' } = this.props
+    return <div className="fb-page" data-href={`https://www.facebook.com/${page}/`} data-tabs={tabs} data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+      <blockquote cite={`https://www.facebook.com/${page}/`} className="fb-xfbml-parse-ignore">
+        <a href={`https://www.facebook.com/${page}/`}>{ name }</a>
+      </blockquote>
+    </div>
+  }
+}
