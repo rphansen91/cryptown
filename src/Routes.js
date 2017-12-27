768c75601739ebf273e895f0a0fc72653d9de07c
Ryan Hansen <rphansen91@gmail.com>
2017-12-26 23:22:05 -0800

    Terms

--- src/Routes.js
+++ src/Routes.js
@@ -8,6 +8,7 @@ import NotFound from './ui/NotFound';
 import GraphiQL from './explorer/GraphiQL';
 import Add from './ui/Add';
 import Coin from './ui/Coin';
+import Terms from './ui/Terms';
 import Transactions from './ui/Txs';
 import { init, pageview } from './utility/analytics';
 
@@ -37,6 +38,7 @@ class Routes extends Component {
         <Route path={(process.env.PUBLIC_URL || '') + "/add"} component={Add} />
         <Route path={(process.env.PUBLIC_URL || '') + "/gql"} component={() => <GraphiQL style={{position: 'relative', height: '80vh', textAlign: 'initial'}}/>} />
         <Route path={(process.env.PUBLIC_URL || '') + "/coin/:id"} component={(props) => <Coin id={props.match.params.id} />} />
+        <Route path={(process.env.PUBLIC_URL || '') + "/terms"} component={Terms} />
         <Route path="*" component={NotFound} />
       </Switch>
     </div>
--- src/gql/connection.js
+++ src/gql/connection.js
@@ -1,2 +1,2 @@
-export const uri = 'https://8nqq2hl581.execute-api.us-east-1.amazonaws.com/dev/graphql'
+export const uri = 'https://laqr73ujn2.execute-api.us-east-1.amazonaws.com/dev/graphql'
 // export const uri = 'http://localhost:3000/graphql'
