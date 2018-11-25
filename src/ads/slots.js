48ef123e2f361db5b9b60cf1abf9f96d083cb581
rphansen91 <rphansen91@gmail.com>
2018-11-24 19:04:35 -0800

    Ad image

--- src/ads/slots.js
+++ src/ads/slots.js
@@ -5,7 +5,7 @@ export const TopBannerDisplayAd = () =>
   <Ad><ins className="adsbygoogle"
     style={{ display: "block" }}
     data-ad-client="ca-pub-7183056818143738"
-    data-ad-slot="9119536926"
+    data-ad-slot="3387340853"
     data-ad-format="auto"
     data-full-width-responsive="true" /></Ad>
 
