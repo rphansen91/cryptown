6bfa14bd41d0fb3396aa83c711325159fd014f73
rphansen91 <rphansen91@gmail.com>
2018-11-30 12:16:21 -0800

    Service worker

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-import { ApolloProvider } from 'react-apollo';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import client from './gql/client';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { compose, createStore, applyMiddleware } from 'redux';
-import blockStore from './store/reducers/index';
-import grey from 'material-ui/colors/grey';
-import thunk from 'redux-thunk';
-import lightBlue from 'material-ui/colors/lightBlue';
-import { BrowserRouter } from 'react-router-dom';
-import storage from './utility/storage';
+import { ApolloProvider } from "react-apollo";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import client from "./gql/client";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { compose, createStore, applyMiddleware } from "redux";
+import blockStore from "./store/reducers/index";
+import grey from "material-ui/colors/grey";
+import thunk from "redux-thunk";
+import lightBlue from "material-ui/colors/lightBlue";
+import { BrowserRouter } from "react-router-dom";
+import storage from "./utility/storage";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = composeEnhancers(applyMiddleware(thunk));
 
 initialStore()
-.then(initial => createStore(blockStore, initial, middleware))
-.then(render);
+  .then(initial => createStore(blockStore, initial, middleware))
+  .then(render);
 
-function initialStore () {
+function initialStore() {
   const initial = window.__INITIAL_STATE__ || {};
-  return storage('profile').get()
-  .then(profile => {
-    initial.profile = profile || {}
-    return initial
-  })
-  .catch(() => initial)
+  return storage("profile")
+    .get()
+    .then(profile => {
+      initial.profile = profile || {};
+      return initial;
+    })
+    .catch(() => initial);
 }
 
-function render (store) {
+function render(store) {
   ReactDOM.render(
     <BrowserRouter>
       <Provider store={store}>
@@ -39,6 +40,9 @@ function render (store) {
           <App />
         </ApolloProvider>
       </Provider>
-    </BrowserRouter>
-  , document.getElementById('root'));
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
 }
+
+registerServiceWorker();
